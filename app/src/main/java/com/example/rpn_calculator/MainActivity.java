c5afd49816cbcf5fee92f680eac6027b455087de
RachelBrisman <rachaelibrisman@gmail.com>
2022-05-12 17:28:15 -0400

    all fixes

--- app/src/main/java/com/example/rpn_calculator/MainActivity.java
+++ app/src/main/java/com/example/rpn_calculator/MainActivity.java
@@ -148,9 +148,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        } else if (id == R.id.action_instructions) {
+        if (id == R.id.action_instructions) {
             showInstructions();
         } else if (id == R.id.action_toggle_save_equation) {
             toggleMenuItem(item);
